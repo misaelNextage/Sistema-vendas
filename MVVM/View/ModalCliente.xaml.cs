b526e75baab5a1beb588fb7ee6b283f6b086ff0e
Misael Orelio <cassi.mo@benner.com.br>
2022-08-19 13:48:10 -0300

    view modal cliente

--- MVVM/View/ModalCliente.xaml.cs
+++ MVVM/View/ModalCliente.xaml.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WpfApp3.MVVM.Model;
 
 namespace WpfApp3.MVVM.View
 {
-    /// <summary>
-    /// Lógica interna para ModalCliente.xaml
-    /// </summary>
     public partial class ModalCliente : Window
     {
         public ModalCliente(ViewModel.PessoaViewModel listParam)
@@ -40,11 +25,6 @@ namespace WpfApp3.MVVM.View
         {
             param.PessoaSelecionada = (Pessoa)datagridPessoasModal.SelectedItem;
             this.Close();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
